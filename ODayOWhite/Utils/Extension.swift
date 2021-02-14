7d582b0b99ba540bb2fbb4c219a8e159d4ea8021
fogbar <kimdongjun6570@gmail.com>
2021-02-14 16:13:48 +0900

    weak self showDataVC에다가 추가

--- ODayOWhite/Utils/Extension.swift
+++ ODayOWhite/Utils/Extension.swift
@@ -10,11 +10,32 @@ import SafariServices
 import MessageUI
 
 
-extension PersonalInfoViewController: MFMailComposeViewControllerDelegate {
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+//extension PersonalInfoViewController: MFMailComposeViewControllerDelegate {
+//    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+//        if let _ = error {
+//            controller.dismiss(animated: true)
+//        }
+//        controller.dismiss(animated: true)
+//    }
+//}
+//
+//extension MainViewController: MFMailComposeViewControllerDelegate{
+//    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+//        if let _ = error {
+//            controller.dismiss(animated: true)
+//        }
+//        
+//        controller.dismiss(animated: true)
+//    }
+//}
+
+
+extension UIViewController: MFMailComposeViewControllerDelegate{
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         if let _ = error {
             controller.dismiss(animated: true)
         }
+        
         controller.dismiss(animated: true)
     }
 }
